21b7da97193cd5ad541b6a08d46836096f563d65
walter <walter@werther-web.de>
2026-05-14 18:21:02 +0200

    clean default values

--- custom_components/ecotracker_emulator/const.py
+++ custom_components/ecotracker_emulator/const.py
@@ -11,12 +11,12 @@ API_PATH = "/v1/json"
 
 # Standard-Fallback-Werte (falls keine Sensoren gewählt)
 DEFAULT_VALUES = {
-    "power": 13,
-    "powerAvg": 2,
-    "agePower": 2635906,
-    "powerPhase1": 83,
-    "powerPhase2": -168,
-    "powerPhase3": 98,
-    "energyCounterOut": 5502204.6,
-    "energyCounterIn": 1540610.7
+    "power": 0,
+    "powerAvg": 0,
+    "agePower": 0,
+    "powerPhase1": 0,
+    "powerPhase2": 0,
+    "powerPhase3": 0,
+    "energyCounterOut": 0,
+    "energyCounterIn": 0
 }
